70d5535e154fba9fc4122edd28ed8e910885e3df
MrGeek-zrh <mrgeek-zrh@outlook.com>
2024-07-19 19:00:04 +0800

    update config

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -5,15 +5,3 @@ vim.opt.wrap = true
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
-
-vim.g.clipboard = {
-  name = "OSC 52",
-  copy = {
-    ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
-    ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
-  },
-  paste = {
-    ["+"] = require("vim.ui.clipboard.osc52").paste("+"),
-    ["*"] = require("vim.ui.clipboard.osc52").paste("*"),
-  },
-}
